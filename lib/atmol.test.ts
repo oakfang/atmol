0694baa282b5244669b14af1a6376c447e5791ba
Alon Niv <oakfang@gmail.com>
2024-06-01 16:41:41 +0300

    add peeking test

--- lib/atmol.test.ts
+++ lib/atmol.test.ts
@@ -1,5 +1,5 @@
 import { test, expect, mock } from "bun:test";
-import { atom, get, set, molecule, wave, async } from ".";
+import { atom, get, set, molecule, wave, async, peek } from ".";
 
 test("atom:get/set", () => {
   const a = atom(0);
@@ -26,6 +26,26 @@ test("waves(atoms)", () => {
 
   expect(get(b)).toBe(2);
   expect(get(c)).toBe(3);
+});
+
+test('peeking does not trigger a wave', () => {
+  const a = atom(0);
+  const b = atom(0);
+  const c = atom(0);
+  wave(() => {
+    set(b, peek(a) + 1);
+  });
+  wave(() => {
+    set(c, peek(b) + 1);
+  });
+
+  expect(get(b)).toBe(1);
+  expect(get(c)).toBe(2);
+
+  set(a, 1);
+
+  expect(get(b)).toBe(1);
+  expect(get(c)).toBe(2);
 });
 
 test("molecule:get/set", () => {
